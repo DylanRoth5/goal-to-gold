2ba8e395ea02734d85c202b44b0aa103d8ddea66
dylanroth <130003124+DylanRoth5@users.noreply.github.com>
2024-08-21 20:51:01 -0300

    changes to home

--- components/Front.tsx
+++ components/Front.tsx
@@ -19,8 +19,9 @@ export default function Front({ email }: FrontProps) {
               :
               <div className="flex flex-col items-center justify-center">
                 <h1 className="text-6xl flex flex-col xl:flex-row font-minecraft text-foreground font-bold">
-                  Welcome to {' '}
-                  <p className="font-semibold align-middle tracking-tight text-foreground pl-2"><span className="ml-1 text-muted-foreground"> Goal</span><span className="text-3xl"> to </span><span className=" text-amber-500">Gold</span></p>
+                   {' '}
+                   <svg fill="none" xmlns="http://www.w3.org/2000/svg" width="44" height="44" className="text-amber-500 translate-x-2" viewBox="0 0 24 24"> <path d="M16 4h-2v2h2v2H6v2H4v8h2v2h6v-2H6v-8h10v2h-2v2h2v-2h2v-2h2V8h-2V6h-2V4z" fill="currentColor"/></svg>
+                  <p className="font-semibold align-middle tracking-tight text-foreground pl-2"><span className="ml-1 text-muted-foreground">Back</span><span className="text-3xl"></span><span className=" text-amber-500">Track</span></p>
                 </h1>
                 <div className="flex">
                   <p className="mt-3 text-2xl font-minecraft text-foreground p-2">
